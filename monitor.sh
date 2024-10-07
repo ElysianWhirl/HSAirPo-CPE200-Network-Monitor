082b4a737f2c8a5aeed2fad5c91efc98a46eb8df
ElysianWhirl <158003859+ElysianWhirl@users.noreply.github.com>
2024-10-07 22:19:39 +0000

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -6,23 +6,15 @@ target2="your_targer_host"
 target3="your_targer_host"
 
 # Define the interval between pings in seconds
-interval=3
+interval=1
 
-# Number of consecutive failed pings to trigger a reset
-consecutive_failures=3
+# Consecutive failed
+consecutive_failed=3
 
-# Function to perform ping
-perform_ping() {
-    local target="$1"
-    local target_name="$2"
-    if ping -c 1 -W 1 "$target" >/dev/null; then
-        echo "$(date "+%Y-%m-%d %H:%M:%S") - USB0 IP: $usb0_ip: Ping to $target_name OK (Response time: $(ping -c 1 "$target" | awk -F'=' '/time=/ {print $4}' | cut -d " " -f 1) ms)"
-        return 0
-    else
-        echo "$(date "+%Y-%m-%d %H:%M:%S") - USB0 IP: $usb0_ip: Ping to $target_name failed"
-        return 1
-    fi
-}
+# Initialize variables to count consecutive failed pings for each target
+failed_count_target1=0
+failed_count_target2=0
+failed_count_target3=0
 
 # Loop indefinitely
 while :
@@ -33,41 +25,56 @@ do
     # Get IP address obtained by usb0 interface
     usb0_ip=$(ip -4 addr show usb0 | awk '/inet / {print $2}' | cut -d "/" -f 1)
 
-    # Initialize failed counts for each target
-    failed_count_target1=0
-    failed_count_target2=0
-    failed_count_target3=0
-
-    # Perform ping for each target
-    perform_ping "$target1" "Target 1"
-    if [ $? -eq 0 ]; then
-        failed_count_target1=0
+    if [ -z "$usb0_ip" ]; then
+        echo "$current_time - USB0 IP not available. Checking again..."
     else
-        failed_count_target1=$((failed_count_target1 + 1))
-    fi
+        all_failed=1
 
-    perform_ping "$target2" "Target 2"
-    if [ $? -eq 0 ]; then
-        failed_count_target2=0
-    else
-        failed_count_target2=$((failed_count_target2 + 1))
-    fi
+        # Perform ping for target 1
+        ping_result=$(ping -c 1 -W 1 $target1 2>/dev/null)
+        if [ $? -eq 0 ]; then
+            response_time=$(echo "$ping_result" | awk -F'=' '/time=/ {print $4}' | cut -d " " -f 1)
+            echo "$current_time - USB0 IP: $usb0_ip: Ping to $target1 OK (Response time: $response_time ms)"
+            failed_count_target1=0
+            all_failed=0
+        else
+            echo "$current_time - USB0 IP: $usb0_ip: Ping to $target1 failed"
+            failed_count_target1=$((failed_count_target1 + 1))
+        fi
 
-    perform_ping "$target3" "Target 3"
-    if [ $? -eq 0 ]; then
-        failed_count_target3=0
-    else
-        failed_count_target3=$((failed_count_target3 + 1))
-    fi
+        # Perform ping for target 2
+        ping_result=$(ping -c 1 -W 1 $target2 2>/dev/null)
+        if [ $? -eq 0 ]; then
+            response_time=$(echo "$ping_result" | awk -F'=' '/time=/ {print $4}' | cut -d " " -f 1)
+            echo "$current_time - USB0 IP: $usb0_ip: Ping to $target2 OK (Response time: $response_time ms)"
+            failed_count_target2=0
+            all_failed=0
+        else
+            echo "$current_time - USB0 IP: $usb0_ip: Ping to $target2 failed"
+            failed_count_target2=$((failed_count_target2 + 1))
+        fi
 
-    # Check if all targets encountered consecutive failed pings
-    if [ $failed_count_target1 -ge $consecutive_failures ] && [ $failed_count_target2 -ge $consecutive_failures ] && [ $failed_count_target3 -ge $consecutive_failures ]; then
-        echo "$current_time - USB0 IP: $usb0_ip: All targets encountered $consecutive_failures consecutive failed pings. Running 4greset.sh..."
-        # Run 4greset.sh script
-        /usr/bin/4greset.sh
-        echo "$current_time - USB0 IP: $usb0_ip: Waiting for 45 seconds before resuming pinging..."
-        # Wait for 45 seconds before resuming pinging
-        sleep 45
+        # Perform ping for target 3
+        ping_result=$(ping -c 1 -W 1 $target3 2>/dev/null)
+        if [ $? -eq 0 ]; then
+            response_time=$(echo "$ping_result" | awk -F'=' '/time=/ {print $4}' | cut -d " " -f 1)
+            echo "$current_time - USB0 IP: $usb0_ip: Ping to $target3 OK (Response time: $response_time ms)"
+            failed_count_target3=0
+            all_failed=0
+        else
+            echo "$current_time - USB0 IP: $usb0_ip: Ping to $target3 failed"
+            failed_count_target3=$((failed_count_target3 + 1))
+        fi
+
+        # Check if all targets encountered consecutive failed pings
+        if [ $all_failed -eq 1 ] && [ $failed_count_target1 -ge $consecutive_failed ] && [ $failed_count_target2 -ge $consecutive_failed ] && [ $failed_count_target3 -ge $consecutive_failed ]; then
+            echo "$current_time - USB0 IP: $usb0_ip: All targets encountered $consecutive_failed consecutive failed pings. Running activate_airplane.sh..."
+            /root/activate_airplane.sh && echo "$current_time - USB0 IP: $usb0_ip: activate_airplane.sh executed successfully" || echo "$current_time - USB0 IP: $usb0_ip: activate_airplane.sh execution failed"
+            echo "$current_time - USB0 IP: $usb0_ip: Waiting for USB0 IP before resuming pinging..."
+            failed_count_target1=0
+            failed_count_target2=0
+            failed_count_target3=0
+        fi
     fi
 
     # Wait for the specified interval before pinging again
